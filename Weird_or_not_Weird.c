60fee1c51598240e0b76d6f09991c5162a7f1af5
itz-sanidhya <119916682+itz-sanidhya@users.noreply.github.com>
2022-12-06 22:31:38 +0530

    #include<stdio.h> int main() {     int n;     scanf("A single line containing a positive integer,%d",&n);     if((n%2!=0)||(n%2==0&&n>6&&n<20)) 	{         printf("weird"); 	}     else if((n%2==0&&n>2&&n<50)||(n%2==0&&n>20)) 	{         printf("not weird");     }
    }

--- Weird_or_not_Weird.c
+++ Weird_or_not_Weird.c
@@ -2,15 +2,14 @@
 int main()
 {
     int n;
-    n>1;
-    n<100;
     scanf("A single line containing a positive integer,%d",&n);
     if((n%2!=0)||(n%2==0&&n>6&&n<20))
-    {
+	{
         printf("weird");
-    }
-     else if((n%2==0&&n>2&&n<5)||(n%2==0&&n>20))
-    {
+	}
+    else if((n%2==0&&n>2&&n<50)||(n%2==0&&n>20))
+	{
         printf("not weird");
     }
+	
 }
