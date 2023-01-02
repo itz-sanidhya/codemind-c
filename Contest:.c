91c35e855f855d88ecd63b8d82041e6a80bd343c
itz-sanidhya <119916682+itz-sanidhya@users.noreply.github.com>
2023-01-02 09:43:58 +0530

    #include<stdio.h> int main() {     int a,b,c,i,j,k;     scanf("%d%d%d",&a,&b,&c);     i=b*1;     j=c*2;     k=i+j;     if(a<=k)     {              printf("Qualify");     }     else     {              printf("Not Qualify");     }
    }

---
+++ Contest:.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,i,j,k;
+    scanf("%d%d%d",&a,&b,&c);
+    i=b*1;
+    j=c*2;
+    k=i+j;
+    if(a<=k)
+    {
+             printf("Qualify");
+    }
+    else
+    {
+             printf("Not Qualify");
+    }
+    
+}
