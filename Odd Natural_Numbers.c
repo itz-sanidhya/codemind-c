a69ed1bcba8405235e40200848706a0d3910c3e9
itz-sanidhya <119916682+itz-sanidhya@users.noreply.github.com>
2023-01-03 18:57:00 +0530

    #include<stdio.h> int main() {     int n,k,i;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(i%2!=0)         {             printf("%d ",i);         }         else         {             continue;         }     } }

---
+++ Odd Natural_Numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,i;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(i%2!=0)
+        {
+            printf("%d ",i);
+        }
+        else
+        {
+            continue;
+        }
+    }
+}
