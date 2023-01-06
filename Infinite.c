7cbbe36ed312eb5770f6c414e4dcb9849f8fcf75
itz-sanidhya <119916682+itz-sanidhya@users.noreply.github.com>
2023-01-06 23:14:35 +0530

    #include<stdio.h> int main() {     int a,b,c,d,i,j,k;     while(a!=-1)     {         scanf("%d",&a);         if(a==-1)         {             break;         }         k=a*a;         printf("%d",k);         printf(" ");     } }

---
+++ Infinite.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,i,j,k;
+    while(a!=-1)
+    {
+        scanf("%d",&a);
+        if(a==-1)
+        {
+            break;
+        }
+        k=a*a;
+        printf("%d",k);
+        printf("
+");
+    }
+}
